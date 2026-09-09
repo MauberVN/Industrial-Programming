d0dbebde5657064748bd0381299f1527f2973f30
Новик Владислав <Novik_VS_25@student.grsu.by>
2026-09-09 15:47:57 +0300

    Реализованы методы ReadData и ReadCommands для работы с файлами

--- Labs/Lab1/Program.cs
+++ Labs/Lab1/Program.cs
@@ -6,22 +6,70 @@ using System.Text;
 
 namespace Lab1
 {
-    struct GeneticData
-    {
-        public string protein;
-        public string organism;
-        public string amino_acids;
-    }
-
-    struct Command
-    {
-        public string name;
-        public string parameter1;
-        public string parameter2;
-    }
-    
     class Program
     {
+        struct GeneticData
+        {
+            public string protein;
+            public string organism;
+            public string amino_acids;
+        }
+
+        struct Command
+        {
+            public string name;
+            public string parameter1;
+            public string parameter2;
+        }
+
+        static List<GeneticData> ReadData(string filename)
+        {
+            List<GeneticData> data = new List<GeneticData>();
+
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (string.IsNullOrEmpty(line)) continue;
+
+                    string[] parts = line.Split('\t');
+
+                    GeneticData gd;
+                    gd.protein = parts[0];
+                    gd.organism = parts[1];
+                    gd.amino_acids = parts[2];
+                    
+                    data.Add(gd);
+                }
+            }
+            return data;
+        }
+
+        static List<Command> ReadCommands(string filename)
+        {
+            List<Command> commands = new List<Command>();
+
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (string.IsNullOrEmpty(line)) continue;
+
+                    string[] parts = line.Split('\t');
+
+                    Command command;
+                    command.name = parts[0];
+                    command.parameter1 = parts.Length > 1 ? parts[1] : String.Empty;
+                    command.parameter2 = parts.Length > 2 ? parts[2] : String.Empty;
+                    
+                    commands.Add(command);
+                }
+            }
+            return commands;
+        }
+
         static void Main(String[] args)
         {
             string sequenceFile = "sequence.0.txt";
