890c53b0b5f4a6f5afafd0d0a8d7cc4d7e22b919
Новик Владислав <Novik_VS_25@student.grsu.by>
2026-09-09 15:28:55 +0300

    Реализвана точка входа Main (TODO) и подключены требуемые пространства имён

---
+++ Labs/Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lab1;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        string sequenceFile = "sequence.0.txt";
+        string commandFile = "command.0.txt";
+        string outputFile = "genedata.0.txt";
+
+        if (args.Length == 3)
+        {
+            sequenceFile = args[0];
+            commandFile = args[1];
+            outputFile = args[2];
+        }
+
+        List<GeneticData> data = ReadData(sequenceFile);
+        List<Command> commands = ReadCommands(commandFile);
+
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            CommandHandler(data, commands, writer);
+        }
+        
+        Console.WriteLine("Ready! The result is written to a file " + outputFile);
+    }
+}
