71e90218f968941901b3104a122ab826955c14a9
Новик Владислав <Novik_VS_25@student.grsu.by>
2026-09-10 13:44:00 +0300

    Реализована точка входа программы Main (TODO)

---
+++ Labs/Lab2/Program.cs
@@ -0,0 +1,15 @@
+﻿using Systeem;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Game game = new Game(16);
+            game.Run();
+
+            Console.WriteLine("Ready. The result was recorded to the file " + Game.OutFile);
+        }
+    }
+}
